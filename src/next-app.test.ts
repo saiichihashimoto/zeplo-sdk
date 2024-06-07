0d10e60f8482baef7d5f668225d5b4e0eb715acd
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2024-06-07 15:11:41 -0500

    fix(next): Revert "fix(next): export app router separately"
    This reverts commit dcf36b396342107b6cfc13fda720e65a7976808e.

--- src/next-app.test.ts
+++ src/next-app.test.ts
@@ -52,7 +52,7 @@ describe("next-app", () => {
               process.nextTick(resolve);
             });
 
-            queueResponse = await queue.handler({
+            queueResponse = await queue({
               text: async () => body,
               headers: new Headers({
                 "x-zeplo-id": "foo",
@@ -433,7 +433,7 @@ describe("next-app", () => {
 
   it("calls queue inline instead of waiting with mode=direct", async () => {
     fetchSpy.mockImplementation(async (input, { body, headers = {} } = {}) => {
-      queueResponse = await queue.handler({
+      queueResponse = await queue({
         text: async () => body,
         headers: new Headers({
           ...(!("X-Zeplo-Id" in headers)
